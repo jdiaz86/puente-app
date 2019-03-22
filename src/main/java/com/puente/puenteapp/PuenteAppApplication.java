5cd69003dfeb35ebf3b03545c14588f2d5b2aa7b
Jorge <jdiaz86@gmail.com>
2019-03-21 22:13:43 -0600

    First commit

---
+++ src/main/java/com/puente/puenteapp/PuenteAppApplication.java
@@ -0,0 +1,13 @@
+package com.puente.puenteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuenteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuenteAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puente/puenteapp/PuenteAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.puente.puenteapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PuenteAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
