f9b7cf46aacb9af1445a5a765a0d6ae507e194ab
Jorge <jdiaz86@gmail.com>
2019-04-09 15:42:58 -0600

    Added Oauth security configuration

---
+++ src/main/java/com/puente/puenteapp/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.puente.puenteapp.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
--- src/main/java/com/puente/puenteapp/controller/RoleController.java
+++ src/main/java/com/puente/puenteapp/controller/RoleController.java
@@ -1,12 +1,11 @@
 package com.puente.puenteapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import com.puente.puenteapp.controller.util.PuenteException;
